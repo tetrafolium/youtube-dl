25279ef05d2c7487f92ad3f2d4653131435021a5
Prateek Papriwal <prateek.papriwal@jp.sony.com>
2016-01-14 19:32:06 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**tests**', '**spi**', '**test**']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
